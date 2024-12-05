3bb84a0a9ea821b72becc04ed641b3548f4b0f4e
evan1liu <evan20050827@gmail.com>
2024-12-04 23:12:51 -0600

    fixed reloading issue in the backend

--- main.py
+++ main.py
@@ -298,4 +298,6 @@ if ENV == 'production':
     
     @app.get("/{full_path:path}")
     async def serve_spa(full_path: str):
+        if full_path.startswith("api/"):
+            raise HTTPException(status_code=404, detail="API route not found")
         return FileResponse("frontend/dist/index.html")
